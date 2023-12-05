3f0afd77c288be8a33bd8406721d356c8d639a83
Ivan Shanin <ivanxshanin@gmail.com>
2023-12-05 14:27:29 +0100

    create follow model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,46 @@
-from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models import (
+    CheckConstraint, CASCADE, F, ForeignKey, Q, UniqueConstraint
+)
 
-# Create your models here.
+from core.models import WithTimestamps
+
+User = get_user_model()
+
+
+class Follow(WithTimestamps):
+    """
+    Subscription of a user to another user on the platform.
+    """
+
+    follower = ForeignKey(
+        to=User,
+        editable=False,
+        on_delete=CASCADE,
+        related_name="following",
+        verbose_name="follower",
+    )
+    influencer = ForeignKey(
+        to=User,
+        editable=False,
+        on_delete=CASCADE,
+        related_name="followers",
+        verbose_name="influencer",
+    )
+
+    class Meta:
+        constraints = (
+            UniqueConstraint(
+                fields=["follower", "influencer"],
+                name="follower & influencer must make a unique pair",
+            ),
+            CheckConstraint(
+                check=~Q(
+                    follower=F("influencer")
+                ),
+                name="cannot follow yourself",
+            ),
+        )
+
+    def __str__(self) -> str:
+        return f"{self.follower} follows {self.influencer}"
